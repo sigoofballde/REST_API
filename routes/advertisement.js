a12eb8d34153fa50a65c15a3fa1de7ab777377de
sigoofballde <princessesgoofball@gmail.com>
2018-11-02 11:39:00 -1000

    Files without node modules
    You will need to install the node modules to make this work, or just download the full zip file.

---
+++ routes/advertisement.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var url = require('url');
+
+/* GET advertisement. */
+router.get('/', function (req, res) {
+    /*API for advertisement. If query is requesting text then we send back URL, if
+    **query is requesting image then we send back image. For real world scenario we would
+    **do request for text &* image from database and then stream those down.*/
+    if (req.query.hasOwnProperty("text")) {
+        res.send("https://www.hire_me.com");
+    }
+    if (req.query.hasOwnProperty("img")) {
+        res.sendFile(path.resolve('./public/image/j.jpg'));
+    }
+});
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.sendFile(path.resolve('./views/index.html'));
+});
+
+module.exports = router;
